b854152cc8c6a7d72fb52821b743eb7e457960bb
shahpreetk <preet25@somaiya.edu>
2020-05-08 14:21:23 +0530

    search and detect button functionality added

--- src/App.js
+++ src/App.js
@@ -18,6 +18,21 @@ const particlesOptions = {
   }
 }
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      input:'',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
   render(){
   return (
     <div className="App">
@@ -25,7 +40,7 @@ class App extends Component {
       <Navigation/>
       <Logo/>
       <Rank />
-      <ImageLinkForm/>
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
       {/*
       <FaceRecognition/>*/}
     </div>
