efae602561a0102da13ee4090654b918ba7f695a
shahpreetk <preet25@somaiya.edu>
2020-05-08 18:37:15 +0530

    routes being added

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
       input:'',
       imageUrl:'',
       box:{},
+      route: 'signin'
     }
   }
 
@@ -66,16 +67,24 @@ class App extends Component {
       .catch(err=>console.log(err));
   }
 
+  onRouteChange = (route) => {
+    this.setState({route: route});
+  }
+
   render(){
   return (
     <div className="App">
     <Particles className='particles' params={particlesOptions}/>
-      <Navigation/>
-      <Signin/>
-      <Logo/>
-      <Rank />
-      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+    <Navigation onRouteChange={this.onRouteChange}/>
+    { this.state.route === 'signin' ?
+      <Signin onRouteChange={this.onRouteChange}/> 
+        :<div> 
+          <Logo/>
+          <Rank />
+          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+          <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+        </div>  
+    }
     </div>
   );
 }
