db931db30ce084ac6d3d3ed740f17a7d4f55c84a
shahpreetk <preet25@somaiya.edu>
2020-05-08 15:06:18 +0530

    in process of api addition and photo connection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React,{Component} from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '3cfa21e7bc06441cafaa993b4fdd6084'
+ });
 
 const particlesOptions = {
   particles: {
@@ -31,6 +37,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log('click');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
   }
 
   render(){
@@ -41,8 +55,7 @@ class App extends Component {
       <Logo/>
       <Rank />
       <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-      {/*
-      <FaceRecognition/>*/}
+      <FaceRecognition/>
     </div>
   );
 }
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+    return (
+        <div className='center'>
+            <img src='https://samples.clarifai.com/face-det.jpg' alt='photo_to_be_detected'/>
+        </div>
+    );
+}
+
+export default FaceRecognition;
