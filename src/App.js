325f1001c429cea0bdbfb5d10beb3c42f992c690
shahpreetk <preet25@somaiya.edu>
2020-05-08 18:24:45 +0530

    sign in added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank';
+import Signin from './components/Signin/Signin';
 import './App.css';
 
 const app = new Clarifai.App({
@@ -70,6 +71,7 @@ class App extends Component {
     <div className="App">
     <Particles className='particles' params={particlesOptions}/>
       <Navigation/>
+      <Signin/>
       <Logo/>
       <Rank />
       <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
