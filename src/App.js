c6f227614668af77e5a6e9a3f7369ad9c0904895
shahpreetk <preet25@somaiya.edu>
2020-05-13 00:14:01 +0530

    cleaning code

--- src/App.js
+++ src/App.js
@@ -25,6 +25,22 @@ const particlesOptions = {
     }
   }
 }
+
+const initialState={
+  input:'',
+      imageUrl:'',
+      box:{},
+      route: 'signin',
+      isSignedIn: false,
+      user:{
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
+}
+
 class App extends Component {
   constructor(){
     super();
@@ -94,6 +110,7 @@ class App extends Component {
           .then(count=>{
             this.setState(Object.assign(this.state.user,{entries:count}))
           })
+          .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
@@ -102,7 +119,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if(route==='signout'){
-      this.setState({isSignedIn:false})
+      this.setState(initialState)
     }else if(route==='home'){
       this.setState({isSignedIn:true})
     }
