03f029d02c94031a5f897493a458f35ded2a5ce4
shahpreetk <preet25@somaiya.edu>
2020-05-13 11:50:17 +0530

    security set

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React,{Component} from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -9,10 +8,6 @@ import Rank from './components/Rank/Rank';
 import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import './App.css';
-
-const app = new Clarifai.App({
-  apiKey: '78eda90c7c4c43a79e6cfbd0d5f2b5b1'
- });
 
 const particlesOptions = {
   particles: {
@@ -93,10 +88,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL, 
-        this.state.input)
+    fetch('http://localhost:3000/imageurl',{
+            method:'post',
+            headers:{'Content-Type':'application/json'},
+            body:JSON.stringify({
+                input:this.state.input
+            })
+        })
+      .then(response => response.json())  
       .then((response) => {
         if(response){
           fetch('http://localhost:3000/image',{
